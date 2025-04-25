d53ad82cffbb06637a1d259a017bbdea23cd0496
Rishabh0319 <rishabhtripathi0319@gmail.com>
2025-04-25 19:07:47 -0400

    [feat]: basic file/folder structure setup is DONE

---
+++ src/components/ImageOverlay/ImageOverlay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageOverlay = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ImageOverlay
---
+++ src/redux/slices/videoSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+}
+
+export const videoSlice = createSlice({
+    name: 'video',
+    initialState,
+    reducers: {
+
+    }
+})
+
+export const { } = videoSlice.actions;
